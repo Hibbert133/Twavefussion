495c34e69610caca2142590d990520c5bd930644
Hibbert <1659144464@qq.com>
2026-01-05 21:09:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# [ISCAS 2025] TWavefussion: Wavelet-based Diffusion with Transformer for Multivariate Time Series Anomaly Detection 
+
+
+This repository contains code for the paper, TWavefussion: Wavelet-based Diffusion with Transformer for Multivariate Time Series Anomaly Detection.
+(The code is being sorted out and we will continue to update it.)
+
+##  Overview
+
+In this paper, TWavefussion, an unsupervised diffusion model for MTS anomaly detection combining wavelet-based diffusion model and transformer autoencoder, is proposed. The wavelet-based diffusion model captures fine-grained local features in the high-frequency components of latent features and helps fuse both local and global MTS features better. Comparative experiments show TWavefussion achieves leading performance on three of four datasets.
+
+## Datasets
+
+1. PSM (PooledServer Metrics) is collected internally from multiple application server nodes at eBay.
+2. SMAP (Soil Moisture Active Passive satellite) also is a public dataset from NASA. 
+3. WADI (Water Distribution) is obtained from 127 sensors of the critical infrastructure system under continuous operations. 
+4. SWAT (Secure Water Treatment) is obtained from 51 sensors of the critical infrastructure system under continuous operations. 
+
+We apply our method on four datasets, the SWAT and WADI datasets, in which we did not upload data in this repository. Please refer to [https://itrust.sutd.edu.sg/](https://itrust.sutd.edu.sg/) and send request to iTrust is you want to try the data.
+
+## How to run
+
+- Train and detect:
+
+> CUDA_VISIBLE_DEVICES={gpu_id} python main.py  --config test.yml  --doc ./{dataset}  --sequence --dataset PSM
+
+For example: CUDA_VISIBLE_DEVICES=0 python main.py  --config test.yml  --doc ./PSM  --sequence --dataset PSM
+
+Then you will train the whole model and will get the reconstructed data and detected score.
+
+## How to run with your own data
+
+- By default, datasets are placed under the "data" folder. If you need to change the dataset, you can modify the dataset path  in the main file. Then you should change the corresponding parameters of diffusion2.py in runners fold, TIMEEMB2.py and TransAE.py.
+- diffusion2.py for wavefussion part.
+-  TIMEEMB2.py and  TransAE.py for Transformer part. Note TransAE.py can adjust the dimension of the latent variable Z.
+
+> python main.py  --'dataset'  your dataset
+
+## Result
+
+We  use dataset PSM for testing demonstration, you can run main.py directly and get the corresponding result.
+
+## References
+Our implementation is based on [TimeADDM](https://github.com/Hurongyao/TIMEADDM). We would like to thank them
+
+## Citation
+Hongjun Sheng, Xinggan Peng, Van Kwan Zhi Koh, Bihan Wen and Zhiping Lin, "TWavefussion: Wavelet-based Diffusion with Transformer for Multivariate Time Series Anomaly Detection", IEEE International Symposium on Circuits and Systems (ISCAS) 2025. [Link](https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=11043617).
+
+## Contact
+HSHENG002@e.ntu.edu.sg
---
+++ functions/denoising.py
@@ -0,0 +1,116 @@
+import torch
+from pytorch_wavelets import DWT1DForward, DWT1DInverse
+
+
+def compute_alpha(beta, t):
+    beta = torch.cat([torch.zeros(1).to(beta.device), beta], dim=0)
+    a = (1 - beta).cumprod(dim=0).index_select(0, t + 1).view(-1, 1, 1)
+    return a
+
+
+def generalized_steps(x, seq, model, b, **kwargs):
+    with torch.no_grad():
+        n = x.size(0)
+        seq_next = [-1] + list(seq[:-1])
+        x0_preds = []
+        xs = [x]
+        for i, j in zip(reversed(seq), reversed(seq_next)):
+            t = (torch.ones(n) * i).to(x.device)
+
+            next_t = (torch.ones(n) * j).to(x.device)
+            at = compute_alpha(b, t.long())
+            at_next = compute_alpha(b, next_t.long())
+            xt = xs[-1].to('cuda')
+            et = model(xt, t)
+            x0_t = (xt - et * (1 - at).sqrt()) / at.sqrt()
+            x0_preds.append(x0_t.to('cpu'))
+            c1 = (
+                kwargs.get("eta", 0) * ((1 - at / at_next) * (1 - at_next) / (1 - at)).sqrt()
+            )
+            c2 = ((1 - at_next) - c1 ** 2).sqrt()
+            xt_next = at_next.sqrt() * x0_t + c1 * torch.randn_like(x) + c2 * et
+
+            xs.append(xt_next.to('cpu'))
+
+    return xs, x0_preds
+
+
+def ddpm_steps(x, seq, model, b, **kwargs):
+    with torch.no_grad():
+        n = x.size(0)
+        seq_next = [-1] + list(seq[:-1])
+        xs = [x]
+        x0_preds = []
+        betas = b
+        for i, j in zip(reversed(seq), reversed(seq_next)):
+            t = (torch.ones(n) * i).to(x.device)
+            next_t = (torch.ones(n) * j).to(x.device)
+            at = compute_alpha(betas, t.long())
+            atm1 = compute_alpha(betas, next_t.long())
+            beta_t = 1 - at / atm1
+            x = xs[-1].to('cuda')
+
+            output = model(x, t.float())
+            e = output
+
+            x0_from_e = (1.0 / at).sqrt() * x - (1.0 / at - 1).sqrt() * e
+            x0_from_e = torch.clamp(x0_from_e, -1, 1)
+            x0_preds.append(x0_from_e.to('cpu'))
+            mean_eps = (
+                (atm1.sqrt() * beta_t) * x0_from_e + ((1 - beta_t).sqrt() * (1 - atm1)) * x
+            ) / (1.0 - at)
+
+            mean = mean_eps
+            noise = torch.randn_like(x)
+            mask = 1 - (t == 0).float()
+            mask = mask.view(-1, 1, 1, 1)
+            logvar = beta_t.log()
+            sample = mean + mask * torch.exp(0.5 * logvar) * noise
+            xs.append(sample.to('cpu'))
+    return xs, x0_preds
+
+def generate_custom_noise_with_transformer(x, transformer):
+    mean_x, scale_factor_x = transformer(x)
+    noise = torch.randn_like(x)
+    custom_noise = noise * scale_factor_x + mean_x
+    return custom_noise
+
+def ddim_steps(x, seq, model, betas, eta=0.0):
+    with torch.no_grad():
+        n = x.size(0)
+        seq_next = [-1] + list(seq[:-1])
+        xs = [x]
+        x0_preds = []
+
+        for i, j in zip(reversed(seq), reversed(seq_next)):
+            t = (torch.ones(n) * i).to(x.device)
+            next_t = (torch.ones(n) * j).to(x.device)
+            at = compute_alpha(betas, t.long())
+            atm1 = compute_alpha(betas, next_t.long())
+            beta_t = 1 - at / atm1
+            x = xs[-1].to('cuda')
+            output = model(x, t.float())
+            e = output
+            x0_from_e = (1.0 / at).sqrt() * x - (1.0 / at - 1).sqrt() * e
+            x0_from_e = torch.clamp(x0_from_e, -1, 1)
+            x0_preds.append(x0_from_e.to('cpu'))
+            # Mean prediction step 
+            mean_eps = (
+                (atm1.sqrt() * beta_t) * x0_from_e + ((1 - beta_t).sqrt() * (1 - atm1)) * x
+            ) / (1.0 - at)
+
+            mean = mean_eps
+            noise = torch.randn_like(x)
+            # Correction step:  ODE 
+            if i > 0:
+                # Here you can insert ODE correction policies, such as minor adjustments via the RK4 method
+                mean = mean + (1 - beta_t).sqrt() * noise  # Simplified Euler step
+
+            mask = 1 - (t == 0).float()
+            mask = mask.view(-1, 1, 1)  
+
+            logvar = beta_t.log()
+            sample = mean + mask * torch.exp(0.5 * logvar) * noise
+            xs.append(sample.to('cpu'))
+
+    return xs, x0_preds
